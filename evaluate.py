3e330a7ef1ca47e1454eb2f7af826a229b631315
jscscloris <45819637+jscscloris@users.noreply.github.com>
2020-05-24 22:50:21 +0800

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
 
     '''====================
     Use your own agent here.'''
-    from game2048.agents import ExpectiMaxAgent as TestAgent
+    from game2048.agents import MyOwnAgent as TestAgent
     '''===================='''
 
     scores = []
