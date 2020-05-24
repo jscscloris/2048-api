bffc0f2ab8fc2a8f0de2308a136b56676c7615cc
jscscloris <45819637+jscscloris@users.noreply.github.com>
2020-05-24 22:53:07 +0800

    Update agents.py

--- game2048/agents.py
+++ game2048/agents.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+import torch.nn as nn
+import torch
 
 class Agent:
     '''Agent Base.'''
@@ -46,3 +47,36 @@ class ExpectiMaxAgent(Agent):
     def step(self):
         direction = self.search_func(self.game.board)
         return direction
+
+dic={0:[0,0,0,0,0,0,0,0,0,0,0],2:[0,0,0,0,0,0,0,0,0,0,1],4:[0,0,0,0,0,0,0,0,0,1,0],8:[0,0,0,0,0,0,0,0,1,0,0],
+     16:[0,0,0,0,0,0,0,1,0,0,0],32:[0,0,0,0,0,0,1,0,0,0,0],64:[0,0,0,0,0,1,0,0,0,0,0],128:[0,0,0,0,1,0,0,0,0,0,0],
+     256:[0,0,0,1,0,0,0,0,0,0,0],512:[0,0,1,0,0,0,0,0,0,0,0],1024:[0,1,0,0,0,0,0,0,0,0,0],2048:[1,0,0,0,0,0,0,0,0,0,0]}
+
+class Net(nn.Module):
+  def __init__(self):
+    super(Net,self).__init__()
+    self.net=nn.Sequential(
+        nn.Linear(16*11,25),
+        nn.ReLU(),
+        nn.Linear(25,12),
+        nn.ReLU(),
+        nn.Linear(12,4),
+        nn.Softmax(dim=1)
+    )
+  def forward(self,x):
+    x=x.view(x.size(0),-1)
+    out=self.net(x)
+    return out
+
+
+class MyOwnAgent(Agent):
+  def step(self):
+    model = Net()
+    model.load_state_dict(torch.load('model.pkl'))
+    bd=np.array(self.game.board)
+    bd=bd.flatten()
+    data=[dic[n] for n in bd]
+    data=torch.Tensor([data]).float()
+    out=model.forward(data)
+    direction = out.argmax()
+    return direction.item()
