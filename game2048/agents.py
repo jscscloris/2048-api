f88fddcad3cc550c6c39b862d43e8a72a15a9c8e
jscscloris <45819637+jscscloris@users.noreply.github.com>
2020-06-21 14:47:38 +0800

    Update agents.py

--- game2048/agents.py
+++ game2048/agents.py
@@ -1,6 +1,8 @@
 import numpy as np
 import torch.nn as nn
 import torch
+import torch.nn.functional as F
+from torch.autograd import Variable
 
 class Agent:
     '''Agent Base.'''
@@ -48,35 +50,104 @@ class ExpectiMaxAgent(Agent):
         direction = self.search_func(self.game.board)
         return direction
 
-dic={0:[0,0,0,0,0,0,0,0,0,0,0],2:[0,0,0,0,0,0,0,0,0,0,1],4:[0,0,0,0,0,0,0,0,0,1,0],8:[0,0,0,0,0,0,0,0,1,0,0],
-     16:[0,0,0,0,0,0,0,1,0,0,0],32:[0,0,0,0,0,0,1,0,0,0,0],64:[0,0,0,0,0,1,0,0,0,0,0],128:[0,0,0,0,1,0,0,0,0,0,0],
-     256:[0,0,0,1,0,0,0,0,0,0,0],512:[0,0,1,0,0,0,0,0,0,0,0],1024:[0,1,0,0,0,0,0,0,0,0,0],2048:[1,0,0,0,0,0,0,0,0,0,0]}
+
+map_table={2**i:i for i in range(1,16)}
+map_table[0]=0
+def grid_ohe(arr):
+  ret=np.zeros(shape=(4,4,16),dtype=float)
+  for r in range(4):
+    for c in range(4):
+      ret[r,c,map_table[arr[r,c]]]=1
+  return ret
+
 
 class Net(nn.Module):
-  def __init__(self):
-    super(Net,self).__init__()
-    self.net=nn.Sequential(
-        nn.Linear(16*11,25),
-        nn.ReLU(),
-        nn.Linear(25,12),
-        nn.ReLU(),
-        nn.Linear(12,4),
-        nn.Softmax(dim=1)
-    )
-  def forward(self,x):
-    x=x.view(x.size(0),-1)
-    out=self.net(x)
-    return out
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(16, 64, kernel_size=(4, 1), padding=(2, 0))
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=(1, 4), padding=(0, 2))
+        self.conv3 = nn.Conv2d(128, 128, kernel_size=(2, 2))
+        self.conv4 = nn.Conv2d(128, 128, kernel_size=(3, 3), padding=(1, 1))
+        self.conv5 = nn.Conv2d(128, 128, kernel_size=(4, 4), padding=(2, 2))
+        self.conv6 = nn.Conv2d(128,128,kernel_size=(2,2))
+        self.fc1 = nn.Linear(128 * 4 * 4, 2048)
+        self.fc2 = nn.Linear(2048, 512)
+        self.fc3 = nn.Linear(512, 4)
+        self.batch_norm1 = nn.BatchNorm1d(128 * 4 * 4)
+        self.batch_norm2 = nn.BatchNorm1d(2048)
+        self.batch_norm3 = nn.BatchNorm1d(512)
+
+        self.initialize()
+
+    def forward(self,x):        
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = F.relu(self.conv5(x))
+        x = F.relu(self.conv6(x))
+        x = x.view(-1, 128 * 4 * 4)
+        x = self.batch_norm1(x)
+        x = F.relu(self.fc1(x))
+        x = self.batch_norm2(x)
+        x = F.relu(self.fc2(x))
+        x = self.batch_norm3(x)
+        x = self.fc3(x)
+
+        return x
+
+    def initialize(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_uniform_(m.weight, mode='fan_in')
+            elif isinstance(m, nn.Linear):
+                nn.init.kaiming_uniform_(m.weight, mode='fan_in')
+
+model_64 = Net()
+model_64 = model_64.cuda()
+model_64.eval()
+model_64.load_state_dict(torch.load('64_epoch_200.pkl'))
+
+model_128 = Net()
+model_128 = model_128.cuda()
+model_128.eval()
+model_128.load_state_dict(torch.load('128_epoch_200.pkl'))
+
+model_256 = Net()
+model_256 = model_256.cuda()
+model_256.eval()
+model_256.load_state_dict(torch.load('256_epoch_500.pkl'))
+
+model_512 = Net()
+model_512 = model_512.cuda()
+model_512.eval()
+model_512.load_state_dict(torch.load('512_epoch_200.pkl'))
+
+model_1024 = Net()
+model_1024 = model_1024.cuda()
+model_1024.eval()
+model_1024.load_state_dict(torch.load('1024_epoch_200.pkl'))
 
 
 class MyOwnAgent(Agent):
-  def step(self):
-    model = Net()
-    model.load_state_dict(torch.load('model.pkl'))
+  def step(self):    
     bd=np.array(self.game.board)
-    bd=bd.flatten()
-    data=[dic[n] for n in bd]
-    data=torch.Tensor([data]).float()
-    out=model.forward(data)
-    direction = out.argmax()
+    bd_ohe=grid_ohe(bd)
+    bd_ohe=np.swapaxes(bd_ohe,0,2)
+    bd_ohe=np.expand_dims(bd_ohe, axis=0)
+    bd_ohe=torch.from_numpy(bd_ohe).float()
+    score=self.game.score
+    bd_ohe=Variable(bd_ohe).cuda()
+    if score<=64:
+      out=model_64(bd_ohe)
+    elif score==128:
+      out=model_128(bd_ohe)
+    elif score==256:
+      out=model_256(bd_ohe)
+    elif score==512:
+      out=model_512(bd_ohe)
+    elif score==1024:
+      out=model_1024(bd_ohe)
+
+    direction = nn.functional.softmax(out,dim=1).argmax()
     return direction.item()
